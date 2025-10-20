7ac89e5cd5a08606c3c8b2997fd5cc5668b7e1a8
ahmed11711 <ahmedsamir11711@gmail.com>
2025-10-20 17:23:16 +0300

    add

--- routes/api.php
+++ routes/api.php
@@ -26,3 +26,4 @@ Route::post('kyc', [KycController::class, 'upload']);
     Route::get('blogs-all', [BlogController::class, 'all']);
     Route::get('service', [ServiceController::class, 'index']);
 });
+require __DIR__.'/admin.php';
