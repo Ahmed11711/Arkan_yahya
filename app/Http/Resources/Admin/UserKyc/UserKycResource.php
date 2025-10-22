4c5ab77545875e788386f13253c33414150b3178
ahmed11711 <ahmedsamir11711@gmail.com>
2025-10-23 00:12:50 +0300

    aa

--- app/Http/Resources/Admin/UserKyc/UserKycResource.php
+++ app/Http/Resources/Admin/UserKyc/UserKycResource.php
@@ -11,9 +11,9 @@ class UserKycResource extends JsonResource
         return [
             'id' => $this->id,
             'user_id' => $this->user_id,
-            'front_id' => $this->img ? asset('storage/app/public/' . $this->front_id) : null,
-            'back_id' => $this->img ? asset('storage/app/public/' . $this->back_id) : null,
-            'face' => $this->img ? asset('storage/app/public/' . $this->face) : null,
+            'front_id' => $this->front_id,
+            'back_id' => $this->back_id,
+            'face' => $this->face,
             'status' => $this->status,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
