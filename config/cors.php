6113bef5e70d7058fc920ce423bd65e8ea9684cb
ahmed11711 <ahmedsamir11711@gmail.com>
2025-10-26 22:46:22 +0300

    aa

--- config/cors.php
+++ config/cors.php
@@ -4,10 +4,20 @@ return [
 
     'paths' => ['*'], // أي مسار
     'allowed_methods' => ['*'], // أي method
-    'allowed_origins' => ['*'], // أي domain
+    'allowed_origins' => [
+        'http://localhost:3000',   // React (اللي بيستخدم الكوكيز)
+        'http://127.0.0.1:3000',   // React أحيانًا بيستخدم الـ IP بدل localhost
+        'http://localhost:5173',   // لو بتستخدم Vite أو Next.js
+        'http://127.0.0.1:5173',   // Vite بالـ IP
+        'http://localhost:8000',   // Laravel محلي
+        'http://127.0.0.1:8000',   // Laravel بالـ IP
+        'https://zayamrock.com/home', // موقع الشريك بدون كوكيز
+    ],
     'allowed_origins_patterns' => ['*'], 
     'allowed_headers' => ['*'], // أي headers
     'exposed_headers' => ['*'], // أي headers ممكن يشوفها المتصفح
     'max_age' => 0,
-    'supports_credentials' => false, // مهم هنا
+    'supports_credentials' => true, // مهم هنا
+
+    //ahmed
 ];
