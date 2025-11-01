f028ca9186405560f0996c2dd6d60f158a669103
ahmed11711 <ahmedsamir11711@gmail.com>
2025-11-02 00:31:13 +0200

    userRank

--- app/Http/Resources/Admin/UserRank/UserRankResource.php
+++ app/Http/Resources/Admin/UserRank/UserRankResource.php
@@ -12,6 +12,10 @@ class UserRankResource extends JsonResource
             'id' => $this->id,
             'user_id' => $this->user->name ?? null,
             'rank' => $this->rank,
+            'count_direct'=>$this->count_direct,
+            'count_indirect'=>$this->count_indirect,
+            'count_direct_active'=>$this->count_direct_active,
+            'count_indirect_active'=>$this->count_indirect_active,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
