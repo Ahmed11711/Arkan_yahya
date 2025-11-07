7fffcf5b421a7430bd521a344806d5d46f95b762
ahmed11711 <ahmedsamir11711@gmail.com>
2025-11-07 16:10:39 +0200

    aa

--- app/Http/Resources/Admin/UserKyc/UserKycResource.php
+++ app/Http/Resources/Admin/UserKyc/UserKycResource.php
@@ -11,12 +11,25 @@ class UserKycResource extends JsonResource
         return [
             'id' => $this->id,
             'user_id' => $this->user_id,
-            'front_id' => $this->front_id,
-            'back_id' => $this->back_id,
-            'face' => $this->face,
+            'front_id' => $this->buildImagePath($this->front_id),
+            'back_id' => $this->buildImagePath($this->back_id),
+            'face' => $this->buildImagePath($this->face),
             'status' => $this->status,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
         ];
+    }
+
+    private function buildImagePath(?string $url): ?string
+    {
+        if (!$url) {
+            return null;
+        }
+
+        // استخراج اسم الملف من المسار الأصلي
+        $filename = basename($url);
+
+        // تكوين المسار الجديد
+        return "https://back.zayamrock.com/storage/app/public/Kyc/" . $filename;
     }
 }
