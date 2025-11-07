b1c9a62bd315cab96b28a3a04b327e74bdb9c42c
ahmed11711 <ahmedsamir11711@gmail.com>
2025-11-07 20:42:23 +0200

    aa

--- app/Http/Requests/Kyc/KycRequest.php
+++ app/Http/Requests/Kyc/KycRequest.php
@@ -13,9 +13,9 @@ class KycRequest extends BaseRequest
     {
         return [
             'user_id'=>'required|exists:users,id',
-            'front_id' => 'required|file|mimes:jpeg,png,jpg,pdf|max:5120',
-            'back_id'  => 'required|file|mimes:jpeg,png,jpg,pdf|max:5120',
-            'face'     => 'required|file|mimes:jpeg,png,jpg|max:4096',
+            'front_id' => 'required|file|mimes:jpeg,png,jpg,enc,pdf|max:5120',
+            'back_id'  => 'required|file|mimes:jpeg,png,jpg,pdf,enc|max:5120',
+            'face'     => 'required|file|mimes:jpeg,png,jpg,enc|max:4096',
 
         ];
     }
